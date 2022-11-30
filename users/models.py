78270772f75e5490ecbdd7b50fb78f21fdbc42d3
Your Name <you@example.com>
2022-11-30 22:23:22 +0700

    Added banned field

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
     profile_pic = models.ImageField(upload_to=f'user-profile/user-{str(email)}', blank=True, default='default/default.jpg')
-    
+    banned = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email 
