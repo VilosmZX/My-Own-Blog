1b33fd550448c5e3c63fc516aa6a7420a911beac
Your Name <you@example.com>
2022-11-30 22:23:47 +0700

    Added delete account path

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profile/<str:username>/', views.profile, name='profile'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('delete-account/', views.delete_account, name='delete_account')
 ]
