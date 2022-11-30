6f1b996901fcc21da66b3e825e8f8f0d2e2f1bd0
Your Name <you@example.com>
2022-11-30 22:02:15 +0700

    Remove uneeded parameter

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
-    profile_pic = models.ImageField(upload_to=f'user-profile/user-{str(email)}', null=True, blank=True, default='default/default.jpg')
+    profile_pic = models.ImageField(upload_to=f'user-profile/user-{str(email)}', blank=True, default='default/default.jpg')
     
 
     def __str__(self):
