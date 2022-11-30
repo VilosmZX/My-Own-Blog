a2449ff496c4088797e8927eeded92e8018dcbfc
Your Name <you@example.com>
2022-11-30 22:02:45 +0700

    Added logout path and update profile path

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
-    path('profile/<str:username>/', views.profile, name='profile')
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,23 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from base.models import Post 
 from .models import CustomUser
 
 @login_required(login_url='login')
-def profile(request: HttpRequest, username=None) -> HttpResponse:
-    posts = Post.objects.filter(author__id=request.user.id)
+def profile(request: HttpRequest, username: str) -> HttpResponse:
+    posts = Post.objects.filter(author__username=username)
+    try:
+        user = CustomUser.objects.get(username=username)
+    except:
+        return HttpResponse('User not found or deleted')
     context = {
-        'posts': posts
-    }
-    
-    if (username is not None): 
-        posts = Post.objects.filter(author__username=username)
-        try:
-            user = CustomUser.objects.get(username=username)
-        except:
-            return HttpResponse('User not found or deleted')
-        context = {
-            'posts': posts
-        }
+        'posts': posts,
+        'user': user
+    }   
     return render(request, 'users/profile.html', context)
+
+@login_required(login_url='login')
+def logout_user(request: HttpRequest) -> HttpResponse:
+    logout(request)
+    return redirect('home')
